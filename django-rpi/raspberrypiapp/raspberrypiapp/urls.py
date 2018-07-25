ad276275c888178f667bff6595562790e0ef3df8
seb1gonzalez <sebastian95gonzalez@gmail.com>
2018-07-25 17:28:06 -0600

    test

--- django-rpi/raspberrypiapp/raspberrypiapp/urls.py
+++ django-rpi/raspberrypiapp/raspberrypiapp/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url, include
-from django.contrib import admin
+from django.contrib.auth.views import login
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', views.homepage),
-    url(r'^our_team/$',views.ourTeam),
-    url(r'^services/$', views.services),
-    url(r'^contact/$',views.contact)
+
 ]
 urlpatterns+= staticfiles_urlpatterns()
