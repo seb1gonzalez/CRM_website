022a4f11c982c47f19811974f6cdfaecbb4dc87b
seb1gonzalez <sebastian95gonzalez@gmail.com>
2018-07-25 13:34:26 -0600

    test

--- django-rpi/raspberrypiapp/raspberrypiapp/settings.py
+++ django-rpi/raspberrypiapp/raspberrypiapp/settings.py
@@ -119,5 +119,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'images'),
+    os.path.join(BASE_DIR,'assets/images'),
+    os.path.join(BASE_DIR,'assets/css'),
 )
